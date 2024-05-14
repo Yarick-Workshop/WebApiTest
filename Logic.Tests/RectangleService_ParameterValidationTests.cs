e15dc85200e7236e0a601e38823548ca6241d026
Yarick-Workshop <techno.man.983@gmail.com>
2024-05-14 14:16:08 +0300

    Added parameter validation tests for RectangleService's methods

---
+++ Logic.Tests/RectangleService_ParameterValidationTests.cs
@@ -0,0 +1,90 @@
+using Common;
+using DAL;
+using Logic;
+using Microsoft.Extensions.Configuration;
+
+using NSubstitute;
+
+namespace Logic.Tests;
+
+[TestClass]
+public class RectangleService_ParameterValidationTests
+{
+    [TestMethod]
+    [DataRow(10.0, 10.0)]
+    [DataRow(11.0, 10.0)]
+    public async Task GetIntersectedAsync_When_MinX_is_More_or_equal_MaxX_Then_Throws_ArgumentException(double minX, double maxX)
+    {
+        var request = new GetRectanglesRequest
+        {
+            MinX = minX,
+            MaxX = maxX,
+
+            MinY = 0,
+            MaxY = 10,
+        };
+
+        using var context = CreateInMemoryContext();
+
+        var service = new RectangleService(context);
+
+        var ex = await  Assert.ThrowsExceptionAsync<ArgumentException>(() => service.GetIntersectedAsync(request));
+
+        Assert.AreEqual("MaxX hast to be > MinX.", ex.Message);
+    }
+
+    [TestMethod]
+    [DataRow(10.0, 10.0)]
+    [DataRow(11.0, 10.0)]
+    public async Task GetIntersectedAsync_When_MinY_is_More_or_equal_MaxY_Then_Throws_ArgumentException(double minY, double maxY)
+    {
+        var request = new GetRectanglesRequest
+        {
+            MinX = 0,
+            MaxX = 10,
+
+            MinY = minY,
+            MaxY = maxY,
+        };
+
+        using var context = CreateInMemoryContext();
+
+        var service = new RectangleService(context);
+
+        var ex = await Assert.ThrowsExceptionAsync<ArgumentException>(() => service.GetIntersectedAsync(request));
+
+        Assert.AreEqual("MaxY hast to be > MinY.", ex.Message);
+    }
+
+#if DEBUG
+    [TestMethod]
+    [DataRow(-1)]
+    [DataRow(0)]
+    [DataRow(1001)]
+    [DataRow(10000)]
+    public async Task GenerateListAsync_When_wrong_rectangle_amount_Then_Throws_ArgumentOutOfRangeException(int amount)
+    {
+        
+        using var context = CreateInMemoryContext();
+
+        var service = new RectangleService(context);
+
+        var ex = await Assert.ThrowsExceptionAsync<ArgumentOutOfRangeException>(() => service.GenerateListAsync(amount));
+
+        Assert.AreEqual("Has to be between 1 and 1000 (Parameter 'amount')", ex.Message);
+    }
+#endif
+
+    private TestDbContext CreateInMemoryContext()
+    {//TODO extract to a separate class and fix in memory DB creation
+        var configuration = Substitute.For<IConfiguration>();
+        configuration.GetConnectionString("WebApiDatabase")
+            .ReturnsForAnyArgs("Filename=:memory:");
+
+        var context = new TestDbContext(configuration);
+
+        //await context.Database.EnsureCreated();
+
+        return context;
+    }
+}
